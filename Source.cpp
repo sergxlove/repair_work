2d4fdafc38757341c644bd9eca196a8095208a1e
sergxlove <sergxlove@mail.ru>
2023-12-11 21:05:53 +0600

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class repair_work
+{
+public:
+private:
+	string name;//название работ
+	string addres;//адрес
+	string name_customer;//фио заказчика
+	string date_begin;//дата начала
+	int duration;//продолжительность
+	int price;//стоимость работ
+};
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	return 0;
+}
